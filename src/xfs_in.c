8fc06a32d205d32b42431353bfe473ae02c97ada
Sven Eden <sven.eden@prydeworx.com>
2019-12-20 23:40:09 +0100

    xfs_in.[hc]: Interpret more inode data, and preload local and extent data. Unpack xattr blocks, too.
    Signed-off-by: Sven Eden <sven.eden@prydeworx.com>

--- src/xfs_in.c
+++ src/xfs_in.c
@@ -5,6 +5,7 @@
 
 #include "common.h"
 #include "device.h"
+#include "globals.h"
 #include "log.h"
 #include "utils.h"
 #include "xfs_in.h"
@@ -12,13 +13,357 @@
 
 #include <string.h>
 
+
+#define DATA_START_V1 0x64
+#define DATA_START_V3 0xB0
+
+
+static int build_data_map( xfs_in* in, uint32_t inode_size, uint8_t const* data ) {
+	size_t start = in->version > 2 ? DATA_START_V3 : DATA_START_V1;
+	size_t end   = in->xattr_off ? start + (in->xattr_off * 8) : inode_size;
+
+	if (ST_DEV == in->data_fork_type) {
+		log_error("Special device data forks (0x%02x) are not supported."
+		          " Ignoring inode.", in->data_fork_type);
+		return -1;
+	} else if (ST_LOCAL == in->data_fork_type) {
+		// Check whether the data part is in bounds
+		size_t last = start + in->file_size - 1;
+		if ( last >= end ) {
+			log_error( "Local data block ends out of bounds at byte %zu/%zu",
+				   last, end );
+			return -1;
+		}
+
+		// Create the buffer
+		in->d_loc_data = malloc(in->file_size);
+		if (NULL == in->d_loc_data) {
+			log_critical( "Unable to allocate %zu bytes for local data block!",
+				      in->file_size );
+			return -1;
+		}
+
+		memcpy(in->d_loc_data, data + start, in->file_size);
+	} else if (ST_EXTENTS == in->data_fork_type) {
+		xfs_ex* curr  = NULL;
+		xfs_ex* next  = NULL;
+		size_t  first = 0; // first byte to read;
+		size_t  last  = 0; // last byte to read
+
+		for ( size_t i = 0; i < in->ext_used; ++i ) {
+			// First check the end of the reading. Don't overshoot!
+			first = start + (16 * i);
+			last  = first + 15;
+			if ( last >= end ) {
+				log_error( "Data extent %zu ends out of bounds at byte %zu/%zu",
+				           i, last, end );
+				return -1;
+			}
+
+			// Then allocate another extent structure
+			next = calloc(1, sizeof(xfs_ex));
+			if (NULL == next) {
+				log_critical("Unable to allocate %lu bytes for data extent!",
+				             sizeof(xfs_ex));
+				return -1;
+			}
+
+			// Can it be read? (Should. Only errors are bugs if either is NULL)
+			if ( -1 == xfs_read_ex(next, data + first) )
+				return -1;
+
+			// Set root or extent chain
+			if (curr)
+				curr->next = next;
+			else
+				in->d_ext_root = next;
+			curr = next;
+			next = NULL;
+		}
+	} else if (ST_BTREE == in->data_fork_type) {
+	} else {
+		log_error("Ignoring inode with unknown data fork type 0x%02x!",
+		          in->data_fork_type);
+		return -1;
+	}
+
+	return 0;
+}
+
+
+static void build_xattr_map( xfs_in* in, uint32_t inode_size, uint8_t const* data ) {
+	size_t start = (in->xattr_off * 8) + in->version > 2 ? DATA_START_V3 : DATA_START_V1;
+	size_t end   = inode_size;
+
+	if (ST_DEV == in->xattr_type_flg) {
+		log_error("Special device xattr forks (0x%02x) are not supported.", in->xattr_type_flg);
+		log_info("%s", " ==> Ignoring extended attributes for this inode!");
+		return;
+	} else if (ST_LOCAL == in->xattr_type_flg) {
+		// Let's get the size of the attributes, first
+		uint16_t xattr_size = get_flip16u(data, start); // The length includes the 4 bytes header
+
+		// Check whether the yattr part is in bounds
+		size_t last = start + xattr_size - 1;
+		if ( last >= end ) {
+			log_error( "Local xattr block ends out of bounds at byte %zu/%zu", last, end );
+			log_info("%s", " ==> Ignoring extended attributes for this inode!");
+			return;
+		}
+
+		// Create the buffer
+		in->x_loc_data = malloc(xattr_size);
+		if (NULL == in->d_loc_data) {
+			log_error( "Unable to allocate %zu bytes for local xattr block!", xattr_size );
+			log_info("%s", " ==> Ignoring extended attributes for this inode!");
+			return;
+		}
+
+		memcpy(in->x_loc_data, data + start, xattr_size);
+		unpack_xattr_data(in);
+		// No check here. xattrs aren't that mission critical!
+	} else if (ST_EXTENTS == in->xattr_type_flg) {
+		xfs_ex* curr  = NULL;
+		xfs_ex* next  = NULL;
+		size_t  first = 0; // first byte to read;
+		size_t  last  = 0; // last byte to read
+
+		for ( size_t i = 0; i < in->ext_used; ++i ) {
+			// First check the end of the reading. Don't overshoot!
+			first = start + (16 * i);
+			last  = first + 15;
+			if ( last >= end ) {
+				log_error( "xattr extent %zu ends out of bounds at byte %zu/%zu",
+				           i, last, end );
+				log_info("%s", " ==> Ignoring remaining extended attributes for this inode!");
+				return;
+			}
+
+			// Then allocate another extent structure
+			next = calloc(1, sizeof(xfs_ex));
+			if (NULL == next) {
+				log_critical("Unable to allocate %lu bytes for xattr extent!", sizeof(xfs_ex));
+				log_info("%s", " ==> Ignoring remaining extended attributes for this inode!");
+				return;
+			}
+
+			// Can it be read? (Should. Only errors are bugs if either is NULL)
+			if ( -1 == xfs_read_ex(next, data + first) ) {
+				log_info("%s", " ==> Ignoring remaining extended attributes for this inode!");
+				return;
+			}
+
+			// Set root or extent chain
+			if (curr)
+				curr->next = next;
+			else
+				in->x_ext_root = next;
+			curr = next;
+			next = NULL;
+		}
+	} else if (ST_BTREE == in->xattr_type_flg) {
+		/* Can they even exist? */
+		log_error("Special device xattr forks (0x%02x) are not supported.", in->xattr_type_flg);
+		log_info("%s", " ==> Ignoring extended attributes for this inode!");
+		return;
+	} else {
+		log_error("Ignoring xattrs with unknown fork type 0x%02x!", in->xattr_type_flg);
+		log_info("%s", " ==> Ignoring extended attributes for this inode!");
+		return;
+	}
+}
+
+static int set_file_mode( xfs_in* in, uint8_t const* data ) {
+	uint16_t mode_bits = *((uint16_t*)data); // No flip needed
+	in->file_modes[0] = (uint8_t)((mode_bits & 0x0e00) >> 9); // 0b0000 111 000 000 000
+	in->file_modes[1] = (uint8_t)((mode_bits & 0x01c0) >> 6); // 0b0000 000 111 000 000
+	in->file_modes[2] = (uint8_t)((mode_bits & 0x0038) >> 3); // 0b0000 000 000 111 000
+	in->file_modes[3] = (uint8_t)((mode_bits & 0x0007)     ); // 0b0000 000 000 000 111
+
+	if ( 0 == (in->file_modes[1] | in->file_modes[2] | in->file_modes[3]) ) {
+		log_error("Filemode is %02o%02o%02o%02o ... ignoring inode!",
+			in->file_modes[0], in->file_modes[1], in->file_modes[2], in->file_modes[3]);
+		return -1;
+	}
+
+	return -1;
+}
+
+
+static int set_file_type( xfs_in* in, uint8_t const* data ) {
+	uint8_t type_nibble = ( data[0] & 0xf0 ) >> 4;
+
+	switch( type_nibble ) {
+		case FT_FIFO:
+			log_warning( "%s", "Ignoring FIFO inode..." );
+			break;
+		case FT_CSD:
+			log_warning( "%s", "Ignoring Character special device..." );
+			break;
+		case FT_DIR:
+			log_warning( "%s", "Ignoring Directory..." );
+			break;
+		case FT_BSD:
+			log_warning( "%s", "Ignoring Block special device..." );
+			break;
+		case FT_FILE:
+			in->file_type = FT_FILE;
+			return 0; // All clear, that's what we want!
+		case FT_SLN:
+			log_warning( "%s", "Ignoring  Symlink..." );
+			break;
+		case FT_SCK:
+			log_warning( "%s", "Ignoring  Socket..." );
+			break;
+		default:
+			log_error( "Ignoring invalid file type 0x%0x!", type_nibble );
+	}
+	return -1;
+}
+
+
+void unpack_xattr_data( xfs_in* in ) {
+	if ( NULL == in->x_loc_data )
+		return; // nothing to do
+
+	uint8_t const* data  = in->x_loc_data; // shortcut
+	uint16_t xattr_size  = get_flip16u(data, 0); // The length includes the 4 bytes header
+	uint8_t  xattr_count = data[2];
+	uint8_t  xattr_padd  = data[3];
+	size_t   offset      = 4; // Start after header
+	xattr*   curr        = NULL;
+	xattr*   next        = NULL;
+
+	for (size_t i = 0; i < xattr_count; ++i) {
+		uint8_t name_len = data[offset];
+		uint8_t val_len  = data[offset + 1];
+		size_t  end_byte = offset + 2 + name_len + xattr_padd + val_len - 1;
+
+		// Make sure we are not out of bounds!
+		if (end_byte >= xattr_size) {
+			log_error( "next xattr entry ends out of bounds at byte %zu/%zu", end_byte, xattr_size );
+			log_info("%s", " ==> Ignoring remaining xattr entries in this block!");
+			return;
+		}
+
+		// Allocate buffers for the name and value
+		// But add 1 byte, as xattr names are not NULL terminated,
+		// and values might, too, come without termination!
+		char* name = calloc(name_len + 1, 1);
+		if (NULL == name) {
+			log_critical("Unable to allocate %lu bytes for xattr name!", name_len + 1);
+			log_info("%s", " ==> Ignoring remaining extended attributes for this inode!");
+			return;
+		}
+		char* value = calloc(val_len + 1, 1);
+		if (NULL == name) {
+			log_critical("Unable to allocate %lu bytes for xattr value!", val_len + 1);
+			log_info("%s", " ==> Ignoring remaining extended attributes for this inode!");
+			FREE_PTR(name);
+			return;
+		}
+		memcpy(name,  data + offset + 3, name_len);
+		memcpy(value, data + offset + 3 + name_len + xattr_padd, val_len);
+
+		// Allocate the new xattr entry
+		next = calloc(1, sizeof(xattr));
+		if (NULL == next) {
+			log_critical("Unable to allocate %zu bytes for xattr entry!", sizeof(xattr));
+			log_info("%s", " ==> Ignoring remaining extended attributes for this inode!");
+			FREE_PTR(name);
+			FREE_PTR(value);
+			return;
+		}
+
+		next->flags = data[offset + 2];
+		next->name  = TAKE_PTR(name);
+		next->value = TAKE_PTR(value);
+
+		if (curr)
+			curr->next = next;
+		else
+			in->xattr_root = next;
+		curr   = next;
+		next   = NULL;
+		offset = end_byte + 1;
+	}
+
+}
+
+
+void xfs_free_in(xfs_in** in) {
+	if ( NULL == in ) {
+		log_critical( "%s", "BUG: Called without 'in' pointer!" );
+		return;
+	}
+	if ( NULL == *in )
+		// Simply nothing to do
+		return;
+
+	// shortcut
+	xfs_in* lin = *in; // [l]ocal in
+
+	if (lin->d_ext_root) {
+		xfs_ex* curr    = lin->d_ext_root;
+		xfs_ex* next    = curr->next;
+		lin->d_ext_root = NULL;
+		while (curr) {
+			free(curr);
+			curr = next;
+			if (curr)
+				next = curr->next;
+		}
+	}
+
+	if (lin->d_loc_data) {
+		FREE_PTR(lin->d_loc_data);
+	}
+
+	if (lin->x_ext_root) {
+		xfs_ex* curr    = lin->x_ext_root;
+		xfs_ex* next    = curr->next;
+		lin->x_ext_root = NULL;
+		while (curr) {
+			free(curr);
+			curr = next;
+			if (curr)
+				next = curr->next;
+		}
+	}
+
+	if (lin->x_loc_data) {
+		FREE_PTR(lin->x_loc_data);
+	}
+
+	if (lin->xattr_root) {
+		xattr* curr     = lin->xattr_root;
+		xattr* next     = curr->next;
+		lin->xattr_root = NULL;
+		while (curr) {
+			FREE_PTR(curr->name);
+			FREE_PTR(curr->value);
+			free(curr);
+			curr = next;
+			if (curr)
+				next = curr->next;
+		}
+	}
+
+	FREE_PTR(*in);
+}
+
+
 int xfs_read_in( xfs_in* in, xfs_sb const* sb, uint8_t const* data ) {
 	if ( NULL == in ) {
-		log_critical( "%s", "BUG: Called without 'in' pointert!" );
+		log_critical( "%s", "BUG: Called without 'in' pointer!" );
+		return -1;
+	}
+	if ( NULL == sb ) {
+		log_critical( "%s", "BUG: Called without 'sb' pointer!" );
 		return -1;
 	}
 	if ( NULL == data ) {
-		log_critical( "%s", "BUG: Called without 'data' pointert!" );
+		log_critical( "%s", "BUG: Called without 'data' pointer!" );
 		return -1;
 	}
 
@@ -39,7 +384,7 @@ int xfs_read_in( xfs_in* in, xfs_sb const* sb, uint8_t const* data ) {
 
 	// Copy CRC32 and UUID, then check the UUID
 	memcpy( in->in_crc32, data + 100,  4 );
-	memcpy( in->sb_UUID,     data + 160, 16 );
+	memcpy( in->sb_UUID,  data + 160, 16 );
 	if ( ( in->version > 2 ) && memcmp( sb->UUID, in->sb_UUID, 16 ) ) {
 		char in_uuid_str[37] = { 0x0 };
 		char sb_uuid_str[37] = { 0x0 };
@@ -51,10 +396,13 @@ int xfs_read_in( xfs_in* in, xfs_sb const* sb, uint8_t const* data ) {
 		return -1; // skip this!
 	}
 
+	// Get file type and permission modes
+	if ( -1 == set_file_type( in, &data[2] ) )
+		return -1; // Has already cried about it.
+	if ( -1 == set_file_mode( in, &data[2] ) )
+		return -1; // dito
+
 	// Now just copy/flip the rest
-	in->type_mode       = *( ( uint16_t* )( data + 2 ) ); // must not be flipped
-	in->file_type       = ( 0xf000 & in->type_mode ) >> 12;
-	in->file_mode       =  0x0fff & in->type_mode;
 	in->data_fork_type  = get_flip8u(  data,   5 );
 	in->num_links_v1    = get_flip16u( data,   6 );
 	in->uid             = get_flip32u( data,   8 );
@@ -79,7 +427,7 @@ int xfs_read_in( xfs_in* in, xfs_sb const* sb, uint8_t const* data ) {
 
 	in->num_xattr_exts  = get_flip16u( data,  80 );
 	in->xattr_off       = get_flip8u(  data,  82 );
-	in->xattr_type_flg  = get_flip8u(  data,  82 );
+	in->xattr_type_flg  = get_flip8u(  data,  83 );
 	in->DMAPI_evnt_flg  = get_flip32u( data,  84 );
 	in->DMAPI_state     = get_flip16u( data,  88 );
 	in->flags           = get_flip32u( data,  90 );
@@ -98,6 +446,16 @@ int xfs_read_in( xfs_in* in, xfs_sb const* sb, uint8_t const* data ) {
 
 	in->btime_ep        = get_flip32u( data, 144 );
 	in->btime_ns        = get_flip32u( data, 148 );
+
+	// Handle file storage (local, extents or btree)
+	if ( -1 == build_data_map(in, sb->inode_size, data) )
+		return -1; // Already told what's wrong
+
+	// Handle xattr storage (local, extents or btree)
+	build_xattr_map(in, sb->inode_size, data);
+	// No check here. xattrs aren't that mission critical!
+
+	// We here? Fine!
 
 	return 0;
 }
--- src/xfs_in.h
+++ src/xfs_in.h
@@ -3,6 +3,7 @@
 #pragma once
 
 
+#include "xfs_ex.h"
 #include "xfs_sb.h"
 
 
@@ -11,71 +12,110 @@
 
 /// @brief enum describing the upper nibble of the file type and permissions field
 typedef enum _file_type {
-	FT_FIFO = 0x0100,
-	FT_CSD  = 0x0200, //!< Character special device
-	FT_DIR  = 0x0400, //!< Directory
-	FT_BSD  = 0x0600, //!< Block special device
-	FT_FILE = 0x0800, //!< Regular file
-	FT_SCK  = 0x0c00, //!< Socket
-	FT_SLN  = 0x0a00  //!< Symlink
+	FT_FIFO = 0x1,
+	FT_CSD  = 0x2, //!< Character special device
+	FT_DIR  = 0x4, //!< Directory
+	FT_BSD  = 0x6, //!< Block special device
+	FT_FILE = 0x8, //!< Regular file
+	FT_SLN  = 0xa, //!< Symlink
+	FT_SCK  = 0xc  //!< Socket
 } e_file_type;
+
+/// @brief enum how data forks and extended attributes are stored
+typedef enum _store_type {
+	ST_DEV     = 0, //!< Special device file (data type only)
+	ST_LOCAL   = 1, //!< Data is resident ("local") in the inode
+	ST_EXTENTS = 2, //!< Array of extent structures follows
+	ST_BTREE   = 3  //!< B+Tree root follows
+} e_store_type;
+
+
+/// @brief structure of one extended attribute
+typedef struct _xattr {
+	uint8_t        flags;
+	char*          name;
+	char*          value;
+	struct _xattr* next;
+} xattr;
 
 
 /// @brief structure of an inode
 typedef struct _xfs_in {
-	char     magic[3];       //!< Bytes   0-  1 : Magic number
-	uint16_t type_mode;      //!< Bytes   2-  3 : File type and mode bits (must not be flipped!)
-	uint8_t  file_type;      //!<    Upper nibble of type_mode
-	uint16_t file_mode;      //!<    Lower 12 bits of type_mode
-	uint8_t  version;        //!< Byte    4      : Version (v5 file system uses v3 inodes)
-	uint8_t  data_fork_type; //!< Byte    5      : Data fork type flag
-	uint16_t num_links_v1;   //!< Bytes   6-  7 : v1 inode numlinks field (not used in v3)
-	uint32_t uid;            //!< Bytes   8- 11 : File owner UID
-	uint32_t gid;            //!< Bytes  12- 15 : File GID
+	char        magic[3];       //!< Bytes   0-  1 : Magic number
+	e_file_type file_type;      //!<    Upper nibble of type_mode
+	uint8_t     file_modes[4];  //!<    Lower 12 bits of type_mode, stored as SUGO
+	uint8_t     version;        //!< Byte    4      : Version (v5 file system uses v3 inodes)
+	uint8_t     data_fork_type; //!< Byte    5      : Data fork type flag
+	uint16_t    num_links_v1;   //!< Bytes   6-  7 : v1 inode numlinks field (not used in v3)
+	uint32_t    uid;            //!< Bytes   8- 11 : File owner UID
+	uint32_t    gid;            //!< Bytes  12- 15 : File GID
 
-	uint32_t num_links_v2;   //!< Bytes  16- 19 : v2+ number of links
-	uint16_t project_id_lo;  //!< Bytes  20- 21 : Project ID (low)
-	uint16_t project_id_hi;  //!< Bytes  22- 23 : Project ID (high)
-	uint16_t inc_on_flush;   //!< Bytes  30- 31 : Increment on flush
+	uint32_t    num_links_v2;   //!< Bytes  16- 19 : v2+ number of links
+	uint16_t    project_id_lo;  //!< Bytes  20- 21 : Project ID (low)
+	uint16_t    project_id_hi;  //!< Bytes  22- 23 : Project ID (high)
+	uint16_t    inc_on_flush;   //!< Bytes  30- 31 : Increment on flush
 
-	uint32_t atime_ep;       //!< Bytes  32- 35 : atime epoch seconds
-	uint32_t atime_ns;       //!< Bytes  36- 39 : atime nanoseconds
-	uint32_t mtime_ep;       //!< Bytes  40- 43 : mtime epoch seconds
-	uint32_t mtime_ns;       //!< Bytes  44- 47 : mtime nanoseconds
-	uint32_t ctime_ep;       //!< Bytes  48- 51 : ctime epoch seconds
-	uint32_t ctime_ns;       //!< Bytes  52- 55 : ctime nanoseconds
+	uint32_t    atime_ep;       //!< Bytes  32- 35 : atime epoch seconds
+	uint32_t    atime_ns;       //!< Bytes  36- 39 : atime nanoseconds
+	uint32_t    mtime_ep;       //!< Bytes  40- 43 : mtime epoch seconds
+	uint32_t    mtime_ns;       //!< Bytes  44- 47 : mtime nanoseconds
+	uint32_t    ctime_ep;       //!< Bytes  48- 51 : ctime epoch seconds
+	uint32_t    ctime_ns;       //!< Bytes  52- 55 : ctime nanoseconds
 
-	uint64_t file_size;      //!< Bytes  56- 63 : File (data fork) size
-	uint64_t file_blocks;    //!< Bytes  64- 71 : Number of blocks in data fork
-	uint32_t ext_size_hint;  //!< Bytes  72- 75 : Extent size hint
-	uint32_t ext_used;       //!< Bytes  76- 79 : Number of data extents used
+	uint64_t    file_size;      //!< Bytes  56- 63 : File (data fork) size
+	uint64_t    file_blocks;    //!< Bytes  64- 71 : Number of blocks in data fork
+	uint32_t    ext_size_hint;  //!< Bytes  72- 75 : Extent size hint
+	uint32_t    ext_used;       //!< Bytes  76- 79 : Number of data extents used
 
-	uint16_t num_xattr_exts; //!< Bytes  80- 81 : Number of extended attribute extents
-	uint8_t  xattr_off;      //!< Byte   82      : Inode offset to xattr (8 byte multiples)
-	uint8_t  xattr_type_flg; //!< Byte   83      : Extended attribute type flag
-	uint32_t DMAPI_evnt_flg; //!< Bytes  84- 87 : DMAPI event mask
-	uint16_t DMAPI_state;    //!< Bytes  88- 89 : DMAPI state
-	uint32_t flags;          //!< Bytes  90- 91 : Flags
-	uint32_t gen_number;     //!< Bytes  92- 95 : Generation number
+	uint16_t    num_xattr_exts; //!< Bytes  80- 81 : Number of extended attribute extents
+	uint8_t     xattr_off;      //!< Byte   82     : Inode offset to xattr (8 byte multiples)
+	uint8_t     xattr_type_flg; //!< Byte   83     : Extended attribute type flag
+	uint32_t    DMAPI_evnt_flg; //!< Bytes  84- 87 : DMAPI event mask
+	uint16_t    DMAPI_state;    //!< Bytes  88- 89 : DMAPI state
+	uint32_t    flags;          //!< Bytes  90- 91 : Flags
+	uint32_t    gen_number;     //!< Bytes  92- 95 : Generation number
 
-	uint32_t nxt_unlnkd_ptr; //!< Bytes  96- 99 : Next unlinked ptr (if inode unlinked) (-1 = NULL in XFS)
+	uint32_t    nxt_unlnkd_ptr; //!< Bytes  96- 99 : Next unlinked ptr (if inode unlinked) (-1 = NULL in XFS)
 
 	/* v3 inodes (v5 file system) have the following fields */
-	uint8_t  in_crc32[4];    //!< Bytes 100-103 : CRC32 checksum for inode
-	uint64_t attr_changes;   //!< Bytes 104-111 : Number of changes to attributes
+	uint8_t     in_crc32[4];    //!< Bytes 100-103 : CRC32 checksum for inode
+	uint64_t    attr_changes;   //!< Bytes 104-111 : Number of changes to attributes
 
-	uint64_t last_log_seq;   //!< Bytes 112-119 : Log sequence number of last update
-	uint64_t ext_flags;      //!< Bytes 120-127 : Extended flags
+	uint64_t    last_log_seq;   //!< Bytes 112-119 : Log sequence number of last update
+	uint64_t    ext_flags;      //!< Bytes 120-127 : Extended flags
 
-	uint32_t cow_ext_size;   //!< Bytes 128-131 : Copy on write extent size hint
-	uint8_t  padding[12];    //!< Bytes 132-143 : Padding for future use
+	uint32_t    cow_ext_size;   //!< Bytes 128-131 : Copy on write extent size hint
+	uint8_t     padding[12];    //!< Bytes 132-143 : Padding for future use
 
-	uint32_t btime_ep;       //!< Bytes 144-147 : btime epoch seconds
-	uint32_t btime_ns;       //!< Bytes 148-151 : btime nanoseconds
-	uint64_t inode_id;       //!< Bytes 152-159 : inode number of this inode
+	uint32_t    btime_ep;       //!< Bytes 144-147 : btime epoch seconds
+	uint32_t    btime_ns;       //!< Bytes 148-151 : btime nanoseconds
+	uint64_t    inode_id;       //!< Bytes 152-159 : inode number of this inode
 
-	uint8_t  sb_UUID[16];    //!< Bytes 160-175 : UUID of the device, stored in the superblock
+	uint8_t     sb_UUID[16];    //!< Bytes 160-175 : UUID of the device, stored in the superblock
+
+	/* Data and xattr information */
+	xfs_ex*     d_ext_root;     //!< First data extent if used for file storage
+	uint8_t*    d_loc_data;     //!< Local data if the file is stored inside the inode
+	xfs_ex*     x_ext_root;     //!< First xattr extent if used for extended attributes
+	uint8_t*    x_loc_data;     //!< Local xattr if the extended attributes are stored inside the inode
+	xattr*      xattr_root;     //!< Root element of the xattr chain
 } xfs_in;
+
+
+/** @brief Unpack in->x_loc_data and extend in->xattr_root with the findings
+  *
+  * Note  : For extents unpacking simply load a block and put it in x_loc_data
+  * Note 2: The function is void, because xattrs aren't mission ciritical. Error? Ignore the stuff!
+  *
+  * @param[in,out] in  pointer to the inode structure to work with
+**/
+void unpack_xattr_data( xfs_in* in );
+
+
+/** @brief Destroy xfs_in structures with this function
+  * @param[out] in pointer to the struct pointer of the inode structure to destroy. Sets *in to NULL.
+**/
+void xfs_free_in(xfs_in** in);
 
 
 /** @brief read inode data from a data block
