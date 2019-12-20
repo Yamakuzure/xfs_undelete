71e09ba0bd5f726569eaab673b4348e59390345f
Sven Eden <sven.eden@prydeworx.com>
2019-12-20 23:37:53 +0100

    Added src/xfs_ex.[hc] for working with XFS extents
    Signed-off-by: Sven Eden <sven.eden@prydeworx.com>

---
+++ src/xfs_ex.c
@@ -0,0 +1,41 @@
+/*******************************************************************************
+ * xfs_ex.c : Functions to work with extent data via struct xfs_ex
+ ******************************************************************************/
+
+
+#include "common.h"
+#include "log.h"
+#include "utils.h"
+#include "xfs_ex.h"
+
+
+int xfs_read_ex(xfs_ex* ex, uint8_t const* data) {
+	if ( NULL == ex ) {
+		log_critical( "%s", "BUG: Called without 'ex' pointer!" );
+		return -1;
+	}
+	if ( NULL == data ) {
+		log_critical( "%s", "BUG: Called without 'data' pointer!" );
+		return -1;
+	}
+
+	ex->is_prealloc = data[0] & 0x80; // == b10000000 aka first bit
+	ex->offset      = flip64((*(uint64_t*)(data + 0) & 0x7ffffffffffffe00) >> 9); // = Bits 2 to 55
+	// 0b01111111 11111111 11111111 11111111 11111111 11111111 11111110 00000000
+	//   12345678 90123456 78901234 56789012 34567890 12345678 90123456 78901234
+	//             1          2          3          4           5          6
+	//          0        1        2        3        4        5        6        7
+	ex->block       = flip64((*(uint64_t*)(data + 6) & 0x01ffffffffffffe0) >> 5); // = Bits 56 to 107
+	// 0b00000001 11111111 11111111 11111111 11111111 11111111 11111111 11100000
+	//   90123456 78901234 56789012 34567890 12345678 90123456 78901234 56789012
+	//    5          6          7          8           9         10         11
+	//          6        7        8        9       10       11       12       13
+	ex->length      = flip32((*(uint32_t*)(data + 12) & 0x001fffff)); // = Bits 108-128
+	// 0b00000000 00011111 11111111 11111111
+	//   78901234 56789012 34567890 12345678
+	//     10         11         12
+	//         12       13       14       15
+
+	return 0;
+}
+
---
+++ src/xfs_ex.h
@@ -0,0 +1,24 @@
+#ifndef PWX_XFS_UNDELETE_SRC_XFS_EX_H_INCLUDED
+#define PWX_XFS_UNDELETE_SRC_XFS_EX_H_INCLUDED 1
+#pragma once
+
+
+/// @brief each extent information is 16 bytes packed, not-aligned.
+typedef struct _xfs_ex {
+	uint8_t  is_prealloc; //!< Bit    1       : ( 1) Flag – Set if extent is preallocated but not yet written, zero otherwise
+	uint64_t offset;      //!< Bits   2 -  55 : (54) Logical offset from the start of the file
+	uint64_t block;       //!< Bits  56 - 107 : (52) Absolute block address of the start of the extent
+	uint32_t length;      //!< Bits 108 - 128 : (21) Number of blocks in the extent
+	struct _xfs_ex* next; //!< Pointer to the next extent following this
+} xfs_ex;
+
+
+/** @brief unpack a 16 bytes extent into its components
+  * @param[out] ex    The xfs_ex structure to fill
+  * @param[in]  data  The data to read
+  * @return 0 on success, -1 one otherwise.
+**/
+int xfs_read_ex(xfs_ex* ex, uint8_t const* data);
+
+
+#endif // PWX_XFS_UNDELETE_SRC_XFS_EX_H_INCLUDED
