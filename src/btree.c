a89d7507aee1b7c1e33a744f6f33cc44d768d301
Sven Eden <sven.eden@prydeworx.com>
2020-01-13 17:55:15 +0100

    Add src/btree.[hc] with structures and functions to help interpreting/reading XFS B+Tree data
    Signed-off-by: Sven Eden <sven.eden@prydeworx.com>

---
+++ src/btree.c
@@ -0,0 +1,9 @@
+/*******************************************************************************
+ * xfs_in.c : Functions to work with inode representations of struct xfs_in
+ ******************************************************************************/
+
+
+#include "btree.h"
+#include "log.h"
+#include "utils.h"
+
